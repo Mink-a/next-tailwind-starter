d18e8c5f824a808ee233a1da47a46cafc1a66c3b
Mink-a <rahulakhant@gmail.com>
2022-09-26 14:09:36 +0630

    darkMode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import DarkMode from "../components/DarkMode";
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-indigo-500">
-    Hello world!
-  </h1>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center flex-col gap-4">
+      <h1 className="text-3xl font-bold underline text-indigo-500 dark:text-cyan-500">
+        Hello world!
+      </h1>
+      <DarkMode />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
